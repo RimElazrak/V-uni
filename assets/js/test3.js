85856c9afa7d7d637326a2995fcda229412a6464
RimElazrak <Rimelazrak2@gmail.com>
2020-07-10 20:54:24 +0100

    hello

--- assets/js/test3.js
+++
@@ -1,7 +0,0 @@
-
-	$("#OP")(function(){
-        var next_button = document.getElementById('OP');
-        if ($("#userName").val() == '' || $("#password").val() == '' || $("#confirm").val() == '') {
-            next_button.style.pointerEvents = 'none';
-		}
-	});
